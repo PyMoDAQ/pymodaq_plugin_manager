530481ec5efd9118a63f8a341ef067530bf60cea
Sébastien Weber <seba.weber@gmail.com>
2020-11-06 22:04:55 +0100

    layout and functionnalities of the plugin manager set
    GUI interface, filtering, displaying info is done

--- pymodaq_plugin_manager/manager.py
+++ pymodaq_plugin_manager/manager.py
@@ -1,42 +1,197 @@
-from distlib.index import PackageIndex
-from distlib.locators import SimpleScrapingLocator
-from hashlib import sha256
-import json
-from .validate import validate_json_plugin_list
+import sys
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtCore import Qt, QVariant, pyqtSlot, pyqtSignal
+from pymodaq.daq_utils import gui_utils as gutils
+from pymodaq_plugin_manager.validate import validate_json_plugin_list
+import numpy as np
 
-#using pip directly https://pip.pypa.io/en/latest/reference/pip_install/#git
+class TableModel(gutils.TableModel):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._selected = [False for ind in range(len(self._data))]
+
+    @property
+    def selected(self):
+        return self._selected
+
+    def flags(self, index):
+        f = super().flags(index)
+        if index.column() == 0:
+            f |= Qt.ItemIsUserCheckable
+        return f
+
+    def data(self, index, role=Qt.DisplayRole):
+        if index.isValid():
+            if role == Qt.DisplayRole or role == Qt.EditRole:
+                if index.column() == 0:
+                    if self._data[index.row()][0] == 'pymodaq_plugins':
+                        dat = 'default'
+                    else:
+                        dat = self._data[index.row()][0].split('pymodaq_plugins_')[1]
+                else:
+                    dat = self._data[index.row()][index.column()]
+                return dat
+            elif role == Qt.CheckStateRole:
+                if index.column() == 0:
+                    if self._selected[index.row()]:
+                        return Qt.Checked
+                    else:
+                        return Qt.Unchecked
+        return QVariant()
+
+    def setData(self, index, value, role):
+        if index.isValid():
+            if role == Qt.EditRole:
+                if self.validate_data(index.row(), index.column(), value):
+                    self._data[index.row()][index.column()] = value
+                    self.dataChanged.emit(index, index, [role])
+                    return True
+
+                else:
+                    return False
+            if role == Qt.CheckStateRole:
+                if index.column() == 0:
+                    self._selected[index.row()] = value == Qt.Checked
+                    self.dataChanged.emit(index, index, [role])
+                    return True
+        return False
 
 
-pypi_index = PackageIndex()
-s = SimpleScrapingLocator(pypi_index.url)
+class FilterProxy(QtCore.QSortFilterProxyModel):
 
-def get_pypi_plugins():
-    plugins = []
-    for ss in pypi_index.search('pymodaq'):
-        if 'pymodaq-plugin' in ss['name']:
-            d = s.locate(ss['name'])
-            if d is not None:
-                plugins.append([{"plugin-name": ss['name'],
-                                 "display-name": ss['name'],
-                                 "version": ss['version'],
-                                 "id": '',
-                                 "repository": d.download_url,
-                                 "description": ss['summary'],
-                                 "author": '',
-                                 "homepage": '',
-                                 }])
-    return plugins
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.textRegExp = QtCore.QRegExp()
+        self.textRegExp.setCaseSensitivity(QtCore.Qt.CaseInsensitive)
+        self.textRegExp.setPatternSyntax(QtCore.QRegExp.Wildcard)
+
+    def filterAcceptsRow(self, sourcerow, parent_index):
+        plugin_index = self.sourceModel().index(sourcerow, 0, parent_index)
+        try:
+            plugin_name = self.sourceModel().data(plugin_index)
+            return self.textRegExp.pattern() in plugin_name
+        except Exception as e:
+            print(e)
+            return True
+
+    def setTextFilter(self, regexp):
+        self.textRegExp.setPattern(regexp)
+        self.invalidateFilter()
 
 
-def get_plugin_sourcefile_id(filename):
-    h = sha256()
-    b = bytearray(128*1024)
-    mv = memoryview(b)
-    with open(filename, 'rb', buffering=0) as f:
-        for n in iter(lambda: f.readinto(mv), 0):
-            h.update(mv[:n])
-    return h.hexdigest()
 
-def get_plugins():
-    return validate_json_plugin_list()
 
+class PluginManager:
+    def __init__(self, parent):
+        self.parent = parent
+        self.parent.setLayout(QtWidgets.QVBoxLayout())
+
+
+        self.plugins_available = validate_json_plugin_list()['pymodaq-plugins']
+        self.plugins_installed = validate_json_plugin_list()['pymodaq-plugins']
+        self.plugins_update = validate_json_plugin_list()['pymodaq-plugins']
+
+        self.setup_UI()
+
+    def setup_UI(self):
+
+        settings_widget = QtWidgets.QWidget()
+        settings_widget.setLayout(QtWidgets.QHBoxLayout())
+        self.plugin_choice = QtWidgets.QComboBox()
+        self.plugin_choice.addItems(['Available', 'Update', 'Installed'])
+        self.plugin_choice.currentTextChanged.connect(self.update_model)
+        self.search_edit = QtWidgets.QLineEdit()
+        self.search_edit.setPlaceholderText("Plugin name")
+        settings_widget.layout().addWidget(self.plugin_choice)
+        settings_widget.layout().addStretch()
+        settings_widget.layout().addWidget(QtWidgets.QLabel('Search:'))
+        settings_widget.layout().addWidget(self.search_edit)
+        settings_widget.layout().addStretch()
+        self.action_button = QtWidgets.QPushButton('Install')
+        self.action_button.setEnabled(False)
+        settings_widget.layout().addWidget(self.action_button)
+
+
+        self.parent.layout().addWidget(settings_widget)
+
+        splitter = QtWidgets.QSplitter(Qt.Vertical)
+
+        self.table_view = gutils.TableView()
+        styledItemDelegate = QtWidgets.QStyledItemDelegate()
+        styledItemDelegate.setItemEditorFactory(gutils.SpinBoxDelegate())
+        self.table_view.setItemDelegate(styledItemDelegate)
+        self.table_view.horizontalHeader().show()
+
+        self.model_available = TableModel([[plugin['plugin-name'],
+                                            plugin['version']] for plugin in self.plugins_available],
+                                          header=['Plugin', 'Version'],
+                                          editable=[False, False])
+        self.model_update = TableModel([[plugin['plugin-name'],
+                                         plugin['version']] for plugin in self.plugins_update],
+                                       header=['Plugin', 'Version'],
+                                       editable=[False, False])
+        self.model_installed = TableModel([[plugin['plugin-name'],
+                                            plugin['version']] for plugin in self.plugins_installed],
+                                          header=['Plugin', 'Version'],
+                                          editable=[False, False])
+
+        model_available_proxy = FilterProxy()
+        model_available_proxy.setSourceModel(self.model_available)
+        self.search_edit.textChanged.connect(model_available_proxy.setTextFilter)
+        self.table_view.setModel(model_available_proxy)
+        self.table_view.setSortingEnabled(True)
+        self.table_view.clicked.connect(self.item_clicked)
+
+        self.info_widget = QtWidgets.QTextBrowser()
+
+        splitter.addWidget(self.table_view)
+        splitter.addWidget(self.info_widget)
+
+        self.parent.layout().addWidget(splitter)
+
+
+    def update_model(self, plugin_choice):
+        self.search_edit.textChanged.disconnect()
+        model_proxy = FilterProxy()
+        if plugin_choice == 'Available':
+            model_proxy.setSourceModel(self.model_available)
+            self.action_button.setText('Install')
+        elif plugin_choice == 'Update':
+            model_proxy.setSourceModel(self.model_update)
+            self.action_button.setText('Update')
+        elif plugin_choice == 'Installed':
+            model_proxy.setSourceModel(self.model_installed)
+            self.action_button.setText('Remove')
+        self.search_edit.textChanged.connect(model_proxy.setTextFilter)
+        self.table_view.setModel(model_proxy)
+        self.item_clicked(model_proxy.index(0,0))
+
+
+    def item_clicked(self, index):
+        self.display_info(index)
+        self.action_button.setEnabled(np.any(index.model().sourceModel().selected))
+
+
+
+    def display_info(self, index):
+        self.info_widget.clear()
+        if self.plugin_choice.currentText() == 'Available':
+            plugin = self.plugins_available[index.model().mapToSource(index).row()]
+        elif self.plugin_choice.currentText() == 'Update':
+            plugin = self.plugins_update[index.model().mapToSource(index).row()]
+        elif self.plugin_choice.currentText() == 'Installed':
+            plugin = self.plugins_installed[index.model().mapToSource(index).row()]
+        self.info_widget.setText(plugin['description'])
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    win = QtWidgets.QMainWindow()
+    win.setWindowTitle('PyMoDAQ Plugin Manager')
+    widget = QtWidgets.QWidget()
+    win.setCentralWidget(widget)
+    prog = PluginManager(widget)
+    win.show()
+    sys.exit(app.exec_())
--- pymodaq_plugin_manager/validate.py
+++ pymodaq_plugin_manager/validate.py
@@ -1,4 +1,5 @@
 from hashlib import sha256
+import pkg_resources
 from jsonschema import validate
 import json
 from distlib.index import PackageIndex
@@ -6,7 +7,8 @@ from distlib.locators import SimpleScrapingLocator
 from pathlib import Path
 #using pip directly https://pip.pypa.io/en/latest/reference/pip_install/#git
 from pytablewriter import MarkdownTableWriter
-import copy
+
+
 pypi_index = PackageIndex()
 s = SimpleScrapingLocator(pypi_index.url)
 
@@ -37,36 +39,36 @@ def get_plugin_sourcefile_id(filename):
             h.update(mv[:n])
     return h.hexdigest()
 
-def get_plugins():
-    return validate_json_plugin_list()
+def get_plugins_from_json():
+    return validate_json_plugin_list()['pymodaq-plugins']
+
+def get_plugins_installed():
+    plugins = get_plugins_from_json()
+    plugins_installed = [entry.load().__name__ for entry in pkg_resources.iter_entry_points('pymodaq.plugins')]
 
 
 def validate_json_plugin_list():
     base_path = Path(__file__).parent.joinpath('src')
-
     with open(str(base_path.joinpath('plugin_list.schema'))) as f:
         schema = json.load(f)
-
     with open(str(base_path.joinpath('PluginList.json'))) as f:
         plugins = json.load(f)
-        validate(instance=plugins, schema=schema)
-
+    validate(instance=plugins, schema=schema)
     return plugins
 
-
-
 def write_plugin_doc():
-    plugins = get_plugins()
+    plugins = get_plugins_from_json()
     base_path = Path(__file__).parent
+
     header_keys = ['display-name', 'author', 'homepage', 'version', 'description']
     header = ['Plugin Name', 'Author', 'Homepage', 'Version', 'Description']
     plugins_tmp = []
 
-    for ind, plug in enumerate(plugins['pymodaq-plugins']):
+    for ind, plug in enumerate(plugins):
         tmp = []
         for k in header_keys:
             if k == 'version':
-                tmp.append()
+                tmp.append(f'<a href="{plug["repository"]}" target="_top">{plug["version"]}</a> ')
             else:
                 tmp.append(plug[k])
         plugins_tmp.append(tmp)
@@ -78,6 +80,7 @@ def write_plugin_doc():
         margin=1
     )
     writer.dump(base_path.joinpath('doc/PluginList.md'))
+
 
 if __name__ == '__main__':
     write_plugin_doc()
