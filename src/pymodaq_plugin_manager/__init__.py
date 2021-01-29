6e603cd11a875b51a56e09269f97f44422726c3d
Sébastien Weber <seba.weber@gmail.com>
2021-01-29 10:27:32 +0100

    Update __init__.py

--- src/pymodaq_plugin_manager/__init__.py
+++ src/pymodaq_plugin_manager/__init__.py
@@ -1,6 +1,6 @@
 import json
 from pathlib import Path
-base_path = Path(__path__[0])
+base_path = Path(__file__).parent
 
 with open(str(base_path.joinpath('data/PluginList.json'))) as f:
     __version__ = json.load(f)['version']
